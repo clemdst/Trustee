b83f65d4178b121e07d75b8b4a4a475bc726ecaf
jordanbmrd <jordanbmrd@gmail.com>
2025-04-05 17:36:15 +0200

    feat: edit next config

--- landing-page/next.config.mjs
+++ landing-page/next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
     unoptimized: true, // Disable image optimization for static export
   },
   output: 'export', // Enable static export
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/trustee-landing-page/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/trustee-landing-page' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Hackathon-From-LLM-To-Agentic-AI/landing-page' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Hackathon-From-LLM-To-Agentic-AI/landing-page' : '',
 };
 
 export default nextConfig;
