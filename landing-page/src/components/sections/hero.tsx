cdfb468454338ebfe5bab3d7f35ae10e9ea750b7
jordanbmrd <jordanbmrd@gmail.com>
2025-04-06 11:09:08 +0200

    feat: update youtube link

--- landing-page/src/components/sections/hero.tsx
+++ landing-page/src/components/sections/hero.tsx
@@ -129,7 +129,7 @@ function HeroImage() {
     >
       <HeroVideoDialog
         animationStyle="from-center"
-        videoSrc="https://youtu.be/hCFoGFfChg8"
+        videoSrc="https://www.youtube.com/watch?v=hCFoGFfChg8"
         thumbnailSrc="se-thumbnail.jpg"
         thumbnailAlt="Hero Video"
         className="border rounded-lg shadow-lg max-w-screen-lg mt-16"
